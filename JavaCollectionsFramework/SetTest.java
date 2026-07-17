807a86119324b9841eaa0d47d1cd5ea26261eb66
shivaanurgpatha-lang <shivaanuragpatha@gmail.com>
2026-07-17 21:11:52 +0530

    Initial-commit

---
+++ JavaCollectionsFramework/SetTest.java
@@ -0,0 +1,23 @@
+package com.java.OOPS.JavaCollectionsFramework;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class SetTest {
+    public static void main(String[] args) {
+        Set s = new HashSet();
+        s.add("Shiva");
+        s.add("Shashank");
+        s.add("Karthik");
+        s.add("Anurag");
+        System.out.println(s);
+        s.size();
+        Iterator i = s.iterator();
+        while (i.hasNext()) {
+            String name =(String) i.next();
+            System.out.println(name.toUpperCase());
+
+        }
+
+    }
+}
