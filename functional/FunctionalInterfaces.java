fb1497e9b00309031854eeb25a95e7f1a859c7b0
shivaanurgpatha-lang <shivaanuragpatha@gmail.com>
2026-07-20 22:27:42 +0530

    initial-commit

---
+++ functional/FunctionalInterfaces.java
@@ -0,0 +1,31 @@
+package com.java.OOPS.functional;
+
+
+import com.java.OOPS.models.Customers;
+
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class FunctionalInterfaces {
+    public static void main(String[] args) {
+        //Predicate
+        Predicate<String> length = (str) -> str.length() >= 5 && str.length() <= 10;
+        System.out.println(length.test("madhudsfasdfasd"));
+
+        Customers customer = new Customers();
+        customer.setId(1111);
+        customer.setName("madhu");
+        customer.setEmail("madhu@gmail.com");
+        customer.setPhoneNo("+91-97427661232");
+        customer.setAddress("Hyderabad");
+
+        Consumer<Customers> printCustomer = (customer1) -> {
+            System.out.println("Id : " + customer1.getId());
+            System.out.println("Name :" + customer1.getName());
+            System.out.println("Email : " + customer1.getEmail());
+            System.out.println("Phone No : " + customer1.getPhoneNo());
+            System.out.println("Address : " + customer1.getAddress());
+        };
+        printCustomer.accept(customer);
+    }
+}
