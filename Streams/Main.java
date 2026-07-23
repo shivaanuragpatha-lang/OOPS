186bd760f7891d9c4e5e48daa27b20a9b625b593
shivaanurgpatha-lang <shivaanuragpatha@gmail.com>
2026-07-23 16:04:20 +0530

    Initial-commit

--- Streams/Main.java
+++
@@ -1,4 +0,0 @@
-package com.java.OOPS.Streams;
-
-public class Main {
-}
---
+++ Streams/Streams.java
@@ -0,0 +1,8 @@
+package com.java.OOPS.Streams;
+
+public class Streams {
+     public static void main(String[] args) {
+         
+
+    }
+}
